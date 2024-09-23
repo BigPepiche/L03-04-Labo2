354fc8d0f8310c3766d667ab307278351db86aa2
Reginald Rancy <reggiehr_30@hotmail.com>
2024-09-23 14:01:39 -0400

    Partie 1 terminee

--- TP2.py
+++ TP2.py
@@ -1,16 +1,33 @@
 """
 TP2 : Système de gestion de livres pour une bibliothèque
 
-Groupe de laboratoire : XX
-Numéro d'équipe :  YY
-Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
+Groupe de laboratoire : L03
+Numéro d'équipe :  04
+Noms et matricules : Reginald Rancy (2393346), Zakaria Soudaki (matricule)
 """
+
+import csv
 
 ########################################################################################################## 
 # PARTIE 1 : Création du système de gestion et ajout de la collection actuelle
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
+fichier = open("collection_bibliotheque.csv", "r")
+bibliotheque = {}
+r = csv.reader(fichier)
+for ligne in r:
+    titre = ligne[0]
+    auteur = ligne[1]
+    date_publication = ligne[2]
+    clée = ligne[3]
+    renseignements = {
+            "titre":titre,
+            "auteur":auteur,
+            "date_publication":date_publication
+    }
+    bibliotheque.update({clée:renseignements})
+
 
 
 
